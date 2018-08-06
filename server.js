74e9d07292336f28e9a913ab5502428d0c5c039b
Calvin Friedman <oscarsemail19@gmail.com>
2018-08-06 13:17:30 -0400

    initial commit

--- server.js
+++ server.js
@@ -10,13 +10,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("./client/build/"));
 }
 // Add routes, both API and view
 // app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 // Start the API server
 app.listen(PORT, function() {
