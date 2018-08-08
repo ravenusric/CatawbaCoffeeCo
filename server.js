09b1c48818dfdce3cd29797ae9ed134dcb3112f0
Calvin Friedman <oscarsemail19@gmail.com>
2018-08-08 15:06:00 -0400

    changes

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("./client/build/"));
 } else {
-    app.use(express.static("./client/public/"))
+    app.use(express.static("./client/public/"));
 }
 // Add routes, both API and view
 // app.use(routes);
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/submissions");
 
 // Start the API server
 app.listen(PORT, function() {
