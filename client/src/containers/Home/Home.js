18083e25b1fd0137148c35fde17c0caeadefe2b2
ravenusric <richie.roney@gmail.com>
2018-08-09 13:43:05 -0400

    updated footer on home.js, updated menu with cleaner styles

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Home.css"
 
+const styles = {
+  divStyle: {
+      background: "green",
+      justifyContent: "flex-end",
+  }
+};
+
 const Home = () => (
   <div className="wrapper">
     <div className="container">
@@ -11,13 +18,20 @@ const Home = () => (
       </div>
     </div>
 
-    <div className="footer">
+    <div style={styles.image} className="footer">
       <p>Address: 117 N Main St, Mt Holly, NC 28120</p>
       <p>
         Hours: Sunday Closed Monday 7AM–8PM Tuesday 7AM–8PM Wednesday 7AM–8PM
         Thursday 7AM–8PM Friday 7AM–9PM Saturday 8AM–8PM
-      </p>
+            </p>
       <p>Phone: (704) 820-6556</p>
+      <a href="https://www.facebook.com/catawbacoffeeco/">
+        <img className="img-responsive1" src={require("../../images/fb.png")} alt="Facebook" width="35" height="35" />
+      </a>
+      <a href="https://www.yelp.com/biz/catawba-coffee-co-mount-holly">
+        <img className="img-responsive2" src={require("../../images/Yelp.png")} alt="Yelp" width="35" height="35" /></a>
+      <a href="https://www.instagram.com/catawbacoffeeco/">
+        <img className="img-responsive3" src={require("../../images/Instagram.png")} alt="Instagram" width="35" height="35" /></a>
     </div>
 
   </div>
