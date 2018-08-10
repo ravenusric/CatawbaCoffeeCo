573a06cba5bf78e1b13517574f4876d39a1fe4bc
Calvin Friedman <oscarsemail19@gmail.com>
2018-08-10 12:51:56 -0400

    fixed database connection

--- client/src/components/Form/FormBtn.js
+++ client/src/components/Form/FormBtn.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const FormBtn = props => (
-  <button {...props} onClick="handleFormSubmit" style={{ float: "middle", marginBottom: 10 }}>
+  <button {...props} style={{ float: "middle", marginBottom: 10 }}>
     {props.children}
   </button>
 );
