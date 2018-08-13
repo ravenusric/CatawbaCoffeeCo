dd55153435e0db7f4dcef54b33c22f568cdfb809
JefferyCarmichael <jeff.c.carmichael@gmail.com>
2018-08-13 14:20:06 -0400

    added footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -10,8 +10,9 @@ const Footer = () => (
       <div id="addr" className="col-8">
         <p>Address: 117 N Main St, Mt Holly, NC 28120</p>
         <p>
-          Hours: Sunday Closed Monday 7AM–8PM Tuesday 7AM–8PM Wednesday 7AM–8PM
-          Thursday 7AM–8PM Friday 7AM–9PM Saturday 8AM–8PM
+        Hours: Sunday Closed | Monday 7AM–8PM | Tuesday 7AM–8PM | Wednesday
+            7AM–8PM | Thursday 7AM–8PM | Friday 7AM–9PM | Saturday 8AM–8PM 
+        
             </p>
         <p>Phone: (704) 820-6556</p>
       </div> {/* addr*/}
