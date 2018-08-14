08aa564ef0a2c9d3066f228d090f5b7a87f41271
Calvin Friedman <oscarsemail19@gmail.com>
2018-08-14 15:02:05 -0400

    added gridsend

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const logger = require("morgan");
 const app = express();
-const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
@@ -20,59 +20,21 @@ if (process.env.NODE_ENV === "production") {
 }
 // Add routes, both API and view
 app.post("/api/submissions", (req, res) => {
-  nodemailer.createTestAccount((err, account) => {
-    if (err) {
-      console.error("Failed to create a testing account. " + err.message);
-      return process.exit(1);
-    }
-
-    console.log("Credentials obtained, sending message...");
-
-    // Create a SMTP transporter object
-    let transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      // port: 587,
-      port: 465,
-      secure: true,
-      auth: {
-        user: "katawbatestemail@gmail.com",
-        pass: "katawbaemail"
-      }
-    });
-
-    // Message object
-    let message = {
-      from: req.body.email,
-      to: "katawbatestemail@gmail.com",
-      subject: "Customer Feedback",
-      text: req.body.email + " Thanks you! \n" + req.body.text
-    };
-    let confirmationMessage = {
-      from: "katawbatestemail@gmail.com",
-      to: req.body.email,
-      subject: "Feedback Confirmation",
-      text: "Thank you for contacting Catawba Coffee Co!"
-    };
-
-    transporter.sendMail(message, (err, info) => {
-      if (err) {
-        console.log("Error occurred. " + err.message);
-        return process.exit(1);
-      }
-      console.log("Message sent: %s", info.messageId);
-      // Preview only available when sending through an Ethereal account
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    });
-    transporter.sendMail(confirmationMessage, (err, info) => {
-      if (err) {
-        console.log("Error occurred. " + err.message);
-        return process.exit(1);
-      }
-      console.log("Message sent: %s", info.messageId);
-      // Preview only available when sending through an Ethereal account
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    });
-  });
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: req.body.email,
+    from: "katawbatestemail@gmail.com",
+    subject: "Customer Feedback",
+    text: "Thank you for contacting Catawba Coffee Co!"
+  };
+  const confirmsg = {
+    to: "katawbatestemail@gmail.com",
+    from: req.body.email,
+    subject: "Customer Feedback",
+    text: req.body.text
+  };
+  sgMail.send(msg);
+  sgMail.send(confirmsg);
 });
 
 // app.use(routes);
